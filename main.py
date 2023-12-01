8d9f1d281504627c91d42e9164aef752ecf9e07c
Kris Hvattum <klhvattum@live.com>
2023-12-01 11:49:24 -0800

    add comments to main.py

--- main.py
+++ main.py
@@ -18,14 +18,18 @@ from features.colors import (
 
 os.system('clear')
 
-print(f'\n\n\n\n{light_text}{magenta_background}Welcome to WORDIE.{default_background}')
+print(f'\n{light_text}{magenta_background}Welcome to WORDIE.{default_background}')
 
+#Ask user if they want to hear directions
 instructions_query = input(f'\nWould you like to hear the {blue_text}directions{light_text}? (y/n) ')
 
+#If they want to hear the directions, print them
 if instructions_query.upper() == 'Y' or instructions_query.upper() == 'YES':
     instructions()
 
+#Ask if they want to start a game
 ready = input(f'\nAre you ready to play {red_text}Wordie{light_text}!? (y/n)')
 
+#If they want to start a game, start it
 if ready == 'y' or ready.upper() == 'YES' or ready.upper() == 'Y':
     game(get_random_word)
